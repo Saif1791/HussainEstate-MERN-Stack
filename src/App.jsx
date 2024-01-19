56afb0404ceae74139a2b29af1d94879ad445727
Saif <Saifsep17@gmail.com>
2024-01-19 21:02:18 +0530

    Added pages and routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,22 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home.jsx";
+import About from "./pages/About.jsx";
+import Profile from "./pages/Profile.jsx";
+import SignIn from "./pages/SignIn.jsx";
+import SignUp from "./pages/SignUp.jsx";
 const App = () => {
   return (
     <div>
-      <h1 className="text-red-800">App</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path="*" element={<Home />}></Route>
+          <Route path="/home" element={<Home />}></Route>
+          <Route path="/about" element={<About />}></Route>
+          <Route path="/signin" element={<SignIn />}></Route>
+          <Route path="/signout" element={<SignUp />}></Route>
+          <Route path="/profile" element={<Profile />}></Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 };
